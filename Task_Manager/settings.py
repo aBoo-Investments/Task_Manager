4481f8f001bc3951c409a48e7658aaca819ccde4
Dušan Šula <info@aboo.cz>
2022-05-09 23:30:38 +0200

    Project continue #2

--- Task_Manager/settings.py
+++ Task_Manager/settings.py
@@ -54,8 +54,7 @@ ROOT_URLCONF = 'Task_Manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,13 +113,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'notes/static/')
-
-
-# Upload files (Images)
-
-MEDIA_URL = 'uploads/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
